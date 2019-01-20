b4d96489e07dbe7bfba75ec4a7d0cb0078f85a14
amiriroy <amiriroy.teach.cs.toronto.ca>
2019-01-19 22:11:36 -0500

    Can post pictures from your phone to the cloud

--- my-app/App.js
+++ my-app/App.js
@@ -1,97 +1,45 @@
-import React from 'react';
-import { Button, Image, View, StyleSheet, Dimensions, Text } from 'react-native';
-import { ImagePicker } from 'expo';
+import {
+  createStackNavigator,
+  createAppContainer
+} from 'react-navigation';
+import ChooseImage from './ChooseImage';
+import ParsedView from './ParsedView';
 
-const styles = StyleSheet.create({
-  container: {
-    borderRadius: 4,
-    borderWidth: 10,
-    borderColor: '#d6d7da',
-    // width:200,
-    // height:200
+const RootStack = createStackNavigator({
+  home: {
+    screen: ChooseImage,
+    navigationOptions: ({ navigation }) => ({
+      title: "Choose Image",
+      headerStyle: {
+        backgroundColor: '#f4511e',
+        height: 30
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        justifyContent: 'center'
+      },
+    }),
   },
-  title: {
-    fontSize: 19,
-    fontWeight: 'bold',
-  },
-  activeTitle: {
-    color: 'red',
-  },
-  button: {
-    color: 'red',
+  parsedView: {
+    screen: ParsedView,
+    navigationOptions: ({ navigation }) => ({
+      title: "Choose Image",
+      headerStyle: {
+        backgroundColor: '#f4511e',
+        height: 30
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        justifyContent: 'center'
+      },
+    }),
   },
 });
 
-export default class ImagePickerExample extends React.Component {
-  state = {
-    image: null,
-  };
+const App = createAppContainer(RootStack);
 
-  render() {
-    let { image } = this.state;
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button 
-          // style={styles.button}
-          color="red"
-          title="Pick an image from camera roll"
-          onPress={this._pickImage}
-        />
-        {image &&
-          <Button
-            title="Upload Image"
-            onPress={this._uploadImage}/>
-        }
-        {image &&
-          <Image 
-          source={{ uri: image }} style={styles.container}
-          width={Dimensions.get('window').width}
-          height={Dimensions.get('window').height-100} />
-          }
-          
-          
-      </View>
-    );
-  }
-
-  _uploadImage(source){
-    const data = new FormData();
-    data.append('name', 'testName'); // you can append anyone.
-    data.append('photo', {
-        uri: source.uri,
-        type: 'image/jpeg', // or photo.type
-        name: 'testPhotoName'
-    });
-
-    fetch('https://xxx/react_image', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'multipart/form-data',
-        },
-        body: data,
-    }).then((response) => response.json())
-        .then((responseJson) => {
-
-           console.log(responseJson);
-
-        }).catch((error) => {
-        console.log("An error occured My Dude");
-    });
-
-}
-  _pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      this.setState({ image: result.uri });
-    }
-  };
-}
-
+export default App;
